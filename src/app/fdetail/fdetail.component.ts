4edeabf6a5aebb8cc22cba2333e2ded8adda3823
brosaz <brosaz@osaxis.fr>
2019-03-26 17:17:48 +0100

    .

--- src/app/fdetail/fdetail.component.ts
+++ src/app/fdetail/fdetail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FlistService } from '../flist.service';
 import { Fiche } from '../fiche';
 
@@ -13,17 +13,16 @@ export class FdetailComponent implements OnInit {
   perso : Fiche;
   list : any;
 
-  constructor(private _route: ActivatedRoute, private _flistService : FlistService) {
+  constructor(private _activatedRoute: ActivatedRoute, private _router: Router, private _flistService : FlistService) {
       this.perso = new Fiche();
    }
 
   ngOnInit() {
     this.getFiche();
-    this.getAllFiches();
   }
 
   getFiche(): void {
-    const id = +this._route.snapshot.paramMap.get('id');
+    const id = +this._activatedRoute.snapshot.paramMap.get('id');
     this._flistService.getFiche(id)
       .subscribe(res => {
 
@@ -37,15 +36,53 @@ export class FdetailComponent implements OnInit {
         this.perso.bars = res.bars;
         this.perso.stuff = res.stuff;
         this.perso.competences = res.competences;
-        
+        this.getAllFiches();
         //console.log(this.perso.stats)
       });
   }
 
   getAllFiches() : void{
     this._flistService.getAllFiches().subscribe(res => {
-      this.list = res;
+      //this.list = res;
+      this.list = [];
+      for(let i = 0 ; i < res.length ; ++i){
+        if(res[i].id == this.perso.id){
+          //console.log(res[i]);
+          //this.list[0] = res[i];
+          if((i-2) >= 0) this.list[0] = res[i-2];
+          else this.list[0] = null;
+          if(i-1 >= 0) this.list[1] = res[i-1];
+          else this.list[1] = null;
+          this.list[2] = res[i];
+          if((i+1) < res.length) this.list[3] = res[i+1];
+          else this.list[3] = null;
+          if(i+2 < res.length) this.list[4] = res[i+2];
+          else this.list[4] = null;
+
+          console.log(this.list);
+        }
+      }
     });
+  }
+
+  goto(num : number) : void{
+    this._flistService.getFiche(num)
+      .subscribe(res => {
+
+        this.perso.id = res.id;
+        this.perso.prenom = res.prenom;
+        this.perso.nom = res.nom;
+        this.perso.age = res.age;
+        this.perso.race = res.race;
+        this.perso.classe = res.classe;
+        this.perso.stats = res.stats;
+        this.perso.bars = res.bars;
+        this.perso.stuff = res.stuff;
+        this.perso.competences = res.competences;
+        this.getAllFiches();
+
+      });
+    this._router.navigate(['/detail/'+num]);
   }
 
 }
